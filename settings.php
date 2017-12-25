029835c2ff160c6439823626c927b7bf8741b371
Rayyan Ahmed Quraishi <rayyanmaster@gmail.com>
2017-12-25 16:20:10 +0530

    sign up and login completed

--- settings.php
+++ settings.php
@@ -2,27 +2,15 @@
 	session_start();
 	$user = $_SESSION["username"];
 
-	$con = mysqli_connect('rayyanaqcom.domaincommysql.com', 'hubuddies', 'hubuddies17','hubuddies');
+	include("./head/connect.php");
+	//connect variable name: $conn
 
-	if(isset($_POST['first_name']) == true || isset($_POST['last_name']) == true || isset($_POST['gender']) == true || isset($_POST['dob']) == true) {
-		if(isset($_POST['first_name']) == true) {
-			$first_name = $_POST['first_name'];
-			$setting_query = mysqli_query($con, "UPDATE users SET first_name='$first_name' WHERE username='$user'");
-		}
-		if(isset($_POST['last_name']) == true) {
-			$last_name = $_POST['last_name'];
-			$setting_query = mysqli_query($con, "UPDATE users SET last_name='$last_name' WHERE username='$user'");
-		}
-		if(isset($_POST['gender']) == true) {
-			$gender = $_POST['gender'];
-			$setting_query = mysqli_query($con, "UPDATE users SET gender='$gender' WHERE username='$user'");
-		}
-		if(isset($_POST['dob']) == true) {
-			$dob = $_POST['dob'];
-			$setting_query = mysqli_query($con, "UPDATE users SET dob='$dob' WHERE username='$user'");
-		}
-		header("location:settings.php");
-	}
+	$userData = mysqli_query($conn, "SELECT FirstName,LastName,Gender,DateOfBirth FROM Users WHERE Username='$user' AND Closed='0'");
+	$get = mysqli_fetch_assoc($userData);
+	$FirstName = $get['FirstName'];
+	$LastName = $get['LastName'];
+	$Gender = $get['Gender'];
+	$DateOfBirth = $get['DateOfBirth'];
 ?>
 <!doctype html>
 <html>
@@ -31,22 +19,45 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="./css/settings.css">
 		<link rel="shortcut icon" href="img/favicon.ico">
+		<script type="text/javascript" src="./js/jquery.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$("#settings").on("submit", function(event){
+					event.preventDefault();
+					var firstName = $("#firstName").val();
+					var lastName = $("#lastName").val();
+					var gender = $('.gender:checked').val();
+					var dob = $("#dob").val();
+					$.post('./ajax/settings.php',{firstName:firstName,lastName:lastName,gender:gender,dob:dob},function(data){
+						$("#error").html(data);
+					});
+				});
+			});
+		</script>
 	</head>
 	<body>
 		<header><a href="home.php">Hubuddies</a></header>
 		<center id="body">
-			<form method="post" class="form" id="sign_up" action="settings.php">
+			<form method="post" class="form" id="settings">
 				<h2 class="selected">Settings</h2><br>
-				<input type="text" placeholder="First Name" class="txt" name="first_name"><br>
-				<input type="text" placeholder="Last Name" class="txt" name="last_name"><br>
-				Gender: <input type="radio" name="gender" value="male" id="male"><label for="male">Male</label>
-				&nbsp;<input type="radio" name="gender" value="female" id="female"><label for="female">Female</label><br>
-				Date of birth: <input type="date" name="dob"><br><br>
+				<?php
+
+				$male="";$female="";
+				if($Gender == "male"){$male="checked";}
+				if($Gender == "female"){$female="checked";}
+
+				echo '
+				<input type="text" placeholder="First Name" class="txt" id="firstName" value="'. $FirstName.'"><br>
+				<input type="text" placeholder="Last Name" class="txt" id="lastName" value="'.$LastName.'"><br>
+				Gender: <input type="radio" name="gender" value="male" id="male" class="gender" '. $male .'><label for="male">Male</label>
+				&nbsp;<input type="radio" name="gender" value="female" id="female" class="gender" '. $female .'><label for="female">Female</label><br>
+				Date of birth: <input type="date" name="dob" id="dob" value="'. $DateOfBirth .'"><br><br>
+				';
+				?>
 				<b id="error"></b><br>
-				<input type="submit" value="Submit" class="submit"><br>
+				<input type="submit" value="Save" class="submit"><br>
 				<a href="http://www.hubuddies.com/home.php" id="skip">Continue</a>
 			</form>
 		</center>
-		<footer></footer>
 	</body>
 </html>
